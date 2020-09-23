4b409d50abb3130b4736d46997ae7255960fc585
nmson <nmson@ecpay.vn>
2020-09-23 14:46:52 +0700

    i18n

---
+++ SpringBoot/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = { "/", "/login1" })
+    public String staticResource(Model model) {
+        return "login1";
+    }
+}
