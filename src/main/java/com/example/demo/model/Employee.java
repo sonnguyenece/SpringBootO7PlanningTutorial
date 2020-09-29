33ed7f584594aa548d3a88f3f7fb744983eb4506
nmson <nmson@ecpay.vn>
2020-09-29 15:10:08 +0700

    jwtSpringBoot

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Employee {
+    private String empNo;
+    private String empName;
+    private String position;
+}
